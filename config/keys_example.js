a7429d2e84f1614931fb4d876716d1ed8aa54659
hulkofsky <hulkofsky@gmail.com>
2019-08-19 13:38:12 +0300

    simple oauth example done

---
+++ config/keys_example.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID: 'your_google_client_id',
+        clientSecret: 'your_google_client_secret'
+    },
+    mongodb: {
+        dbURI: 'mongodb://localhost:27017/lambda'
+    },
+    session: {
+        cookieKey: 'your_cookie_key'
+    }
+};
